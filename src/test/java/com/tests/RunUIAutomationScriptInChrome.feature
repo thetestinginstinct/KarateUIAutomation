fe4e487c8556f025e5fc08cfb91b8c989eac4dd6
thetestinginstinct <thetestinginstinct@gmail.com>
2024-03-17 20:33:06 +0530

    Updated RunUIAutomationScriptInChrome.feature file

--- src/test/java/com/tests/RunUIAutomationScriptInChrome.feature
+++ src/test/java/com/tests/RunUIAutomationScriptInChrome.feature
@@ -8,6 +8,8 @@ Feature: Open Swag Labs login page in Chrome Feature
   #To use chromedriver with full-path version 122
   #* configure driver = { type: 'chromedriver', executable: 'C:/Users/akhil/eclipse-workspace/KarateUIAutomation/src/test/java/BrowserDrivers/chromedriver.exe'}
   #To use chrome driver version 122 added in System path variable
-  #* configure driver = { type: 'chromedriver', executable: 'chromedriver' }
+  * configure driver = { type: 'chromedriver', executable: 'chromedriver' }
+  
   Scenario: Open Swag Labs login page in Chrome Scenario
     Given driver 'https://www.saucedemo.com/'
+    * delay(2000)
---
+++ target/chrome_1710687153435/Default/Extensions/fheoggkfdfchfphceeifdbepaooicaho/8.1.0.4852_0/scripts/content_site_listener.js
@@ -0,0 +1,24 @@
+/*!
+ * 
+ *     MCAFEE RESTRICTED CONFIDENTIAL
+ *     Copyright (c) 2024 McAfee, LLC
+ *
+ *     The source code contained or described herein and all documents related
+ *     to the source code ("Material") are owned by McAfee or its
+ *     suppliers or licensors. Title to the Material remains with McAfee
+ *     or its suppliers and licensors. The Material contains trade
+ *     secrets and proprietary and confidential information of McAfee or its
+ *     suppliers and licensors. The Material is protected by worldwide copyright
+ *     and trade secret laws and treaty provisions. No part of the Material may
+ *     be used, copied, reproduced, modified, published, uploaded, posted,
+ *     transmitted, distributed, or disclosed in any way without McAfee's prior
+ *     express written permission.
+ *
+ *     No license under any patent, copyright, trade secret or other intellectual
+ *     property right is granted to or conferred upon you by disclosure or
+ *     delivery of the Materials, either expressly, by implication, inducement,
+ *     estoppel or otherwise. Any license under such intellectual property rights
+ *     must be expressed and approved by McAfee in writing.
+ *
+ */(()=>{"use strict";const e="FOCUS_OR_CREATE_TAB",n="SHOW_SIDEBAR_MAIN",t="PING_CONTENT_SITE_LISTENER",o=0,r=23,s=24,i=0;const a=0,c=0,d=1,l=2,u=3,m=4,f=1,g=2,h=3,p=4,F={BACKGROUND:"BACKGROUND",CONTENT:"CONTENT",TELEMETRY:"TELEMETRY"},E={DEFAULT:"color: #000000; font-weight: normal; font-style:normal; background: #FFFFFF;",BACKGROUND:"color: #8D0DBA; font-weight: bold; background: #FFFFFF;",CONTENT:"color: #F54A26; font-weight: bold; background: #FFFFFF;",TELEMETRY:"color: #147831; font-weight: bold; background: #FFFFFF;"};class w{static log(e,n=null){b(e,f,n)}static error(e,n=null){b(e,g,n)}static warn(e,n=null){b(e,h,n)}static debug(e,n=null){b(e,p,n)}}const b=(e,n,t)=>{const o=a;if(o===c)return;let r="chrome-extension:"===location.protocol?F.BACKGROUND:F.CONTENT;t&&F[t]&&(r=t);const s=new Date,i=n===g?e:`%c[${r} ${s.toLocaleString([],{hour:"2-digit",minute:"2-digit",hour12:!0})}]: %c${e}`,w=E.DEFAULT;let b=E[r];if(b||(b=w),o>=l&&n===g&&console.error(e),o>=d&&n===f&&console.log(i,b,w),o>=u&&n===h){const e="color: #FFA500; font-family: sans-serif; font-weight: bolder; text-shadow: #000 1px 1px;";console.log(`%cWARN - ${i}`,e,b,w)}if(o>=m&&n===p){const e="color: #FF33D7; font-family: sans-serif; font-weight: bolder; text-shadow: #000 1px 1px;";console.log(`%cDEBUG - ${i}`,e,b,w)}};class y{constructor(e){this.pingCommand=e,this.basePingListener(),this.addIdentifier()}basePingListener(e=null){((e,n=null,t)=>{"function"==typeof t?chrome.runtime.onMessage.addListener(((o,r,s)=>{if(r.id===chrome.runtime.id&&"object"==typeof o&&!Array.isArray(o)&&o?.ipcId===e)return t({promises:n,request:o,sender:r,sendResponse:s})})):w.error("Provided with invalid callback function")})("WA",null,(({request:n,sendResponse:t})=>{const{command:o}=n;if(o===this.pingCommand)return w.debug(`File Injection [pinged]: Received ${o} command`),t({content:!0}),"function"==typeof e&&e(),!0}))}addIdentifier(){var e;e=()=>{const e=document.createElement("span");e.id=this.pingCommand,e.style.cssText="display:none",document.body.appendChild(e)},"undefined"!=typeof document&&null!==document&&("complete"===document.readyState||"loading"!==document.readyState&&!document.documentElement.doScroll?e():document.addEventListener("DOMContentLoaded",e))}}const T=async(e,n,t,o)=>{try{return await A(e,n,t,o),!0}catch(e){return w.warn(`[broadcast] Unexpected error when calling command: "${n}", err: ${e.message}`),null}},A=(e,n,t,o,r=null)=>{if(!chrome.tabs)throw new Error('"tabs" permission not set in manifest.');return chrome.tabs.sendMessage(o,{ipcId:e,command:n,...t},{frameId:r})},N=(e,n={},t)=>(async(e,n,t={},o={})=>{try{if(o?.tabId){const{tabId:r,frameId:s}=o;return await A(e,n,t,r,s)}if(o?.broadcast){const r=await chrome.tabs.query({}),{broadcastIgnoreId:s=[]}=o;return r.filter((({id:e})=>!s.includes(e))).forEach((({id:o})=>{T(e,n,t,o)})),!0}return await chrome.runtime.sendMessage({ipcId:e,command:n,...t})}catch(e){return w.warn(`Unexpected error when calling command: "${n}", err: ${e.message}`),null}})("WA",e,n,t);(new class extends y{constructor(){super(t)}main(){window.addEventListener("message",(t=>{if(!t)return;const{data:a}=t;if(!a)return;const{request_type:c,payload:d}=a;if(void 0!==c&&void 0!==d){if(c!==o||d.done||window.postMessage({request_type:i,payload:{done:!0}},t.origin),c===r){const n=chrome.runtime.getURL("html/settings.html");N(e,{url:n})}c===s&&N(n)}}),!1)}}).main()})();
+//# sourceMappingURL=../sourceMap/chrome/scripts/content_site_listener.js.map
